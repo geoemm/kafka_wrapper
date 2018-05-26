f957595d62bb47ffbd64ea206e5b9e9775e8c150
Georgios <geo.emm@hotmail.com>
2018-05-26 20:14:42 +0300

    Initial commit

---
+++ src/main/java/com/emmanoug/kw/KafkaWrapperApplication.java
@@ -0,0 +1,12 @@
+package com.emmanoug.kw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWrapperApplication.class, args);
+	}
+}
