4f6fb84c5dfc6f24a6fbaa5e37422b490b90b76d
Georgios <geo.emm@hotmail.com>
2018-10-06 13:53:18 +0300

    ignore app.yml

--- src/main/java/com/gemmano/kw/controller/KafkaController.java
+++ src/main/java/com/gemmano/kw/controller/KafkaController.java
@@ -1,5 +1,11 @@
 package com.gemmano.kw.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +24,16 @@ public class KafkaController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public String kafkaMsg(@RequestBody DeviceData deviceData) {
+		
+		try {
+	        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+			Date date1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(deviceData.getTimestamp());
+			Date newDate = DateUtils.addHours(date1, 3);
+			deviceData.setTimestamp(dateFormat.format(newDate));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 		kafkaSender.send(deviceData);
 		return "OK";
 	}
